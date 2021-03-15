342a905c769b4bf9809d3fd06e06d0561d8c23f2
Christopher Bryan Padilla-Vallejo <al2143002342@azc.uam.mx>
2021-03-15 00:46:54 -0600

    Update MySQL.class.php

--- MySQL.class.php
+++ MySQL.class.php
@@ -1,137 +1,128 @@
 <?php
-/*
-*   MYSQL: A basic PHP class/template to interact with a MySQL/MariaDB database.
-*
-*   Author: Christopher Bryan Padilla-Vallejo
-*   
-*   You are completely free to use, copy, modify and distribute this code for any purposes (commercial or not)
-*   as long as this header is left intact.
-*/
-
-// Disable error messages (for security reasons). Enable the line in production
+// Una vez en producción, habilitar la siguiente línea
 //error_reporting(0);
 
 class MySQL
 {
-    // Connection credentials
+    // Credenciales para realizar la conexión
     private $host;
     private $port;
     private $database;
     private $user;
     private $password;
-    // Useful variables for the use of this class
     private $connection;
+    // Mensajes de error
     private $last_error_message;
-    public $affected_rows; // Note that this is PUBLIC (for being used outside of this class)
-    // Common errors and descriptions
-    private $errors = array(
-        'INVALID_CONNECTION' => 'Unable to connect with the database, verify your parameters.',
-        'BAD_CONSTRUCTOR' => 'Wrong order or number of parameters.',
-        'SET_ENCODING' => 'Cannot set character encoding.',
-        'PREPARE_STATEMENT' => 'Error while trying to prepare statement.',
-        'BAD_BINDING' => 'Bad parameter binding.',
-        'EXECUTE' => 'Query was not executed.'
+	private static $error_messages = array(
+        'BAD_CONSTRUCTOR' => 'Número de parámetros de conexión incorrecto. Se esperaban 5 pero se han dado ',
+		'BAD_ARGUMENT' => 'Argumento de conexión no válido o fuera de orden: ',
+		'NOT_CONNECTED' => 'No se pudo conectar con la base de datos',
+        'PREPARE_STATEMENT' => 'No se pudo preparar la consulta con la sentencia dada',
+        'BAD_BINDING' => 'Los parámetros dados no coinciden con sus tokens',
+        'EXECUTE' => 'Hubo un error al intentar ejecutar la consulta',
+		'QUERY_DATA' => 'La información proporcionada debe ser un array asociativo'
     );
 
-    /*  ===# CONSTRUCTOR
-    *   [ Returns ]
-    *       + A new instance of the class
-    */
+    /*--------------------------------------------------------------------------------------------------
+	*	CONSTRUCTOR DE LA CLASE
+	*---------------------------------------------------------------------------------------------------*/
     function __construct()
     {
-        $this->last_error_message = '';
-        // Simulating constructor polymorphism
+		$this->connection = null;
+		$this->last_error_message = '';
+        // Simulando polimorfismo del constructor
         switch(func_num_args())
         {
-            case 0: // Default credentials
-                $this->host = "**********";
-                $this->port = **********;
-                $this->database = "**********";
-                $this->user = "**********";
-                $this->password = "**********";
+			// Credenciales por defecto
+            case 0:
+                $this->host = '127.0.0.1';
+                $this->port = 3306;
+                $this->database = 'mysql';
+                $this->user = 'root';
+                $this->password = '';
             break;
-
-            case 5: // Use custom credentials (follows the same order as they're declared)
+			
+			// Usar credenciales alternativas (siguen el mismo orden en que se declararon en la clase)
+            case 5:
                 $args = func_get_args();
                 $this->MySQL($args[0], $args[1], $args[2], $args[3], $args[4]);
             break;
-
-            default: // Invalid number or order of credentials
-                $this->last_error_message = $this->errors['BAD_CONSTRUCTOR'];
+			
+			// Número incorrecto de parámetros
+            default:
+                $this->last_error_message = self::$error_messages['BAD_CONSTRUCTOR'].func_num_args();
             break;
         }
     }
 
-    /*  ===# SET CUSTOM CREDENTIALS TO CONNECT WITH THE DATABASE
-    *   [ Parameters ]
-    *       + IP of the server (String)
-    *       + Port of the server (Int)
-    *       + Name of the database (String)
-    *       + User (String)
-    *       + Password (String)
-    */
+    /*--------------------------------------------------------------------------------------------------
+	*	ESTABLECER CREDENCIALES DE CONEXIÓN PERSONALIZADAS
+	*---------------------------------------------------------------------------------------------------*/
     private function MySQL($_host, $_port, $_database, $_user, $_password)
     {
-        $this->host = $_host;
-        $this->port = $_port;
-        $this->database = $_database;
-        $this->user = $_user;
+		// Validaciones de cada uno de los parámetros, se dejan las llaves por si se agregan más instrucciones
+		if(filter_var($_host, FILTER_VALIDATE_IP)) {
+			$this->host = $_host;
+		} else {
+			$this->last_error_message = self::$error_messages['BAD_ARGUMENT'].'Host';
+		}
+        if(is_integer($_port)) {
+			$this->port = $_port;
+		} else {
+			$this->last_error_message = self::$error_messages['BAD_ARGUMENT'].'Port';
+		}
+		if(strlen(trim($_database))>0 && strpos($_database, ' ')===false) {
+			$this->database = $_database;
+		} else {
+			$this->last_error_message = self::$error_messages['BAD_ARGUMENT'].'Database';
+		}
+		if(strlen(trim($_user))>0 && strpos($_user, ' ')===false) {
+			$this->user = $_user;
+		} else {
+			$this->last_error_message = self::$error_messages['BAD_ARGUMENT'].'User';
+		}
         $this->password = $_password;
     }
 
-    /*  ===# GET THE DETAILS ABOUT THE LAST GENERATED ERROR
-    *   [ Returns ]
-    *       + Error description (String)
-    */
-    public function getErrorDescription()
+    /*--------------------------------------------------------------------------------------------------
+	*	OBTENER EL ÚLTIMO ERROR GENERADO
+	*---------------------------------------------------------------------------------------------------*/
+    public function errorMessage()
     {
-        return $this->last_error_message.PHP_EOL;
+        return $this->last_error_message.'.'.PHP_EOL;
     }
 
-    /*  ===# OPEN A NEW CONNECTION WITH THE DATABASE USING THE SPECIFIED CREDENTIALS
-    *   [ Returns ]
-    *       + Could the connection be stablished? (Boolean)
-    */
+    /*--------------------------------------------------------------------------------------------------
+	*	ABRIR UNA CONEXIÓN USANDO LAS CREDENCIALES PROPORCIONADAS
+	*---------------------------------------------------------------------------------------------------*/
     public function open()
     {
-        // Check if there's problems with the constructor
+        // Validamos que no haya problemas en el constructor
         if(strcmp($this->last_error_message, '') != 0)
             return false;
-        // Attempt to start the connection
+        // Intento de conexión
         $this->connection = mysqli_connect($this->host, $this->user, $this->password, $this->database, $this->port);
-        if(!$this->connection) {
+        if(mysqli_connect_errno()) {
             $this->connection = null;
-            $this->last_error_message = $this->errors['INVALID_CONNECTION'].mysqli_connect_error();
+            $this->last_error_message = self::$error_messages['NOT_CONNECTED'].mysqli_connect_error();
             return false;
         }
-        // Set character encoding
-        if(mysqli_set_charset($this->connection, "utf8mb4") !== true) {
-            mysqli_close($this->connection);
-            $this->connection = null;
-            $this->last_error_message = $this->errors['SET_ENCODING'];
-            return false;
-        }
+        // Establecer la codificación UTF-8
+        @mysqli_set_charset($this->connection, "utf8mb4");
         return true;
     }
 
-    /*  ===# SANATIZES A TEXT VARIABLE FOR SAFE USE
-    *   [ Parameters ]
-    *       + Variable to be sanitized (String)
-    *   [ Returns ]
-    *       + Sanitized variable
-    */
+    /*--------------------------------------------------------------------------------------------------
+	*	LIMPIA UNA VARIABLE PARA USARLA DE MANERA SEGURA (SE APLICA AUTOMÁTICAMENTE)
+	*---------------------------------------------------------------------------------------------------*/
     public function clear($variable)
     {
-        $variable = filter_var(addslashes(strip_tags(trim($variable))), FILTER_SANITIZE_STRING);
-        return $variable;
+        return htmlentities(strip_tags(trim($variable)), ENT_QUOTES|ENT_HTML5|ENT_SUBSTITUTE, 'UTF-8');
     }
 
-    /*  ===# PRIVATE FUNCTION TO BUILD THE "PARAMS-STRING" TO BE BINDED
-    *   [ Parameters ]
-    *       + Statement parameters (Array)
-    *   [ Returns ]
-    *       + String to be binded with the provided parameters (String)
-    */
+    /*--------------------------------------------------------------------------------------------------
+	*	CONSTRUIR LA "CADENA DE TIPOS DE DATO" PARA CONSULTAS PARAMETRIZADAS
+	*---------------------------------------------------------------------------------------------------*/
     private function getDataTypes($params)
     {
         $result = '';
@@ -144,18 +135,14 @@ class MySQL
         return $result;
     }
 
-    /*  ===# FACTORIZED CODE TO PREPARE STATEMENTS (PRIVATE FUNCTION FOR USE OF THIS CLASS ONLY)
-    *   [ Parameters ]
-    *       + SQL statement (String)
-    *       + (Optional) Parameters of the SQL statement (Array)
-    *   [ Returns ]
-    *       + Prepared statement (Object)
-    */
+    /*--------------------------------------------------------------------------------------------------
+	*	PREPARAR UNA CONSULTA PARAMETRIZADA
+	*---------------------------------------------------------------------------------------------------*/
     private function prepareStatement($sql, &$params)
     {
         $prepared_statement = mysqli_prepare($this->connection, $sql);
         if(!$prepared_statement) {
-            $this->last_error_message = $this->errors['PREPARE_STATEMENT'].mysqli_error($this->connection);
+            $this->last_error_message = self::$error_messages['PREPARE_STATEMENT'].mysqli_error($this->connection);
             return null;
         }
         if($params != null) {
@@ -163,44 +150,32 @@ class MySQL
                 $i = $this->clear($i);
             $bind = $this->getDataTypes($params);
             if(!mysqli_stmt_bind_param($prepared_statement, $bind, ...$params)) {
-                $this->last_error_message = $this->errors['BAD_BINDING'].mysqli_error($this->connection);
+                $this->last_error_message = self::$error_messages['BAD_BINDING'].mysqli_error($this->connection);
                 return null;
             }
         }
         return $prepared_statement;
     }
 
-    /*  ===# EXECUTE A QUERY WHICH DON'T RETURNS RECORDS
-    *   [ Parameters ]
-    *       + SQL statement (String)
-    *       + (Optional) Parameters of the SQL statement (Array)
-    *   [ Returns ]
-    *       + Was the statement executed successfuly? (Boolean)
-    *
-    *   > This function also updates the "affected_rows" class attribute
-    */
+    /*--------------------------------------------------------------------------------------------------
+	*	CONSULTA PARAMETRIZADA QUE DEVUELVE EL NÚMERO DE REGISTROS AFECTADOS
+	*---------------------------------------------------------------------------------------------------*/
     public function execute($sql, $params=null)
     {
         $statement = $this->prepareStatement($sql, $params);
         if(!$statement)
-            return false;
-        $this->affected_rows = 0;
+            return -1;
         if(!mysqli_stmt_execute($statement)) {
-            $this->last_error_message = $this->errors['EXECUTE'].mysqli_error($this->connection);
-            return false;
+            $this->last_error_message = self::$error_messages['EXECUTE'].mysqli_error($this->connection);
+            return -1;
         }
-        $this->affected_rows = mysqli_affected_rows($this->connection);
         mysqli_stmt_close($statement);
-        return true;
+        return mysqli_affected_rows($this->connection);
     }
 
-    /*  ===# PERFORM A QUERY WHICH RETURNS RECORDS
-    *   [ Parameters ]
-    *       + SQL statement (String)
-    *       + (Optional) Parameters of the SQL statement (Array)
-    *   [ Returns ]
-    *       + Result set (Object)
-    */
+    /*--------------------------------------------------------------------------------------------------
+	*	CONSULTA PARAMETRIZADA QUE DEVUELVE UN DATASET
+	*---------------------------------------------------------------------------------------------------*/
     public function query($sql, $params=null)
     {
         $statement = $this->prepareStatement($sql, $params);
@@ -208,7 +183,7 @@ class MySQL
             return null;
         $result = null;
         if(!mysqli_stmt_execute($statement)) {
-            $this->last_error_message = $this->errors['EXECUTE'].mysqli_error($this->connection);
+            $this->last_error_message = self::$error_messages['EXECUTE'].mysqli_error($this->connection);
             return null;
         }
         $result = mysqli_stmt_get_result($statement);
@@ -216,16 +191,17 @@ class MySQL
         return $result;
     }
 
-    /*  ===# GET THE CONNECTION INSTANCE (TO USE IT OUTSIDE OF THIS CLASS)
-    *   [ Returns ]
-    *       + Connection (Object)
-    */
-    function getConnection()
+    /*--------------------------------------------------------------------------------------------------
+	*	OBTENER EL CANAL DE CONEXIÓN (PARA USARLO FUERA DE LA CLASE)
+	*---------------------------------------------------------------------------------------------------*/
+    public function getConnection()
     {
         return $this->connection;
     }
-
-    /*  ===# CLOSE THE CONNECTION */
+	
+    /*--------------------------------------------------------------------------------------------------
+	*	CERRAR LA CONEXIÓN
+	*---------------------------------------------------------------------------------------------------*/
     public function close()
     {
         if($this->connection) {
@@ -234,7 +210,9 @@ class MySQL
         }
     }
     
-    /*  ===# CLEAN IT ALL */
+    /*--------------------------------------------------------------------------------------------------
+	*	DESTRUCTOR
+	*---------------------------------------------------------------------------------------------------*/
     function __destruct()
     {
         $this->close();
